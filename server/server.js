e651dc5887dfab558ff8dd2d2742b00c5d496576
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-06 13:49:17 +0530

    Added sample route and middleware

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ const express = require('express')
 // App init
 const app = express();
 
+// Middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
+// Route 
+app.get('/', (req, res) => {
+  res.json({ mssg: "Welcome to the app" });
+})
+
 // Listening for requests
 app.listen(process.env.PORT, () => {
   console.log('Listening to port', process.env.PORT);
