d039c047f3f3fcfa47ce2e1d1b586175ce0c3bcc
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-24 21:44:06 +0530

    fetched data from server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 
 function App() {
   return <div className="App">
---
+++ client/src/components/WorkoutDetails/WorkoutDetails.jsx
@@ -0,0 +1,16 @@
+import classes from "./WorkoutDetails.module.css";
+
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div className={classes.workout_details}>
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
